df14dccb4a0727f3cfedefb8efd638877d5197d6
JairoHansBaeza <jairohansaiep@gmail.com>
2022-12-21 00:35:47 -0300

    UI-Login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'super-gest';
+  constructor(private fb: FormBuilder){}
+
+  loginForm = new FormGroup({
+    usuario: new FormControl('pija',Validators.required),
+    contraseña: new FormControl('paja',Validators.required),
+  })
+
+  ngOnInit(){
+
+  }
+  get usuario(){
+    return this.loginForm.get('usuario');
+  }
 }
